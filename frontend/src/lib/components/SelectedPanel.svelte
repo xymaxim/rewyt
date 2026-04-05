be37313aba0bd69a20b4160d7102792565e2f118
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2026-04-05 23:26:18 +0300

    refactor: reorder class names

--- frontend/src/lib/components/SelectedPanel.svelte
+++ frontend/src/lib/components/SelectedPanel.svelte
@@ -75,7 +75,7 @@
 <div class="mr-5">
   <div
     title="Jump to selected"
-    class="group flex flex-row cursor-pointer"
+    class="group flex cursor-pointer flex-row"
     onclick={() => seekOrRewind(explorer.selectedTime!)}
   >
     <span class="text-timestamp text-sm">
