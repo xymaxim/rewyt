de8caa28b25cfa7fccb8936b8666bedf14f51f96
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2026-04-13 00:27:39 +0300

    fix: clear stream reference on start failure

--- app_local.go
+++ app_local.go
@@ -18,7 +18,7 @@ func newStream(ctx context.Context, videoID string, port int) (stream.Streamer, 
 		Port:        port,
 		SegmentsDir: "./local/dash/segments/",
 		MPDDelay:    0,
-		StartDelay:  0,
+		StartDelay:  10,
 		StreamStart: 200,
 	}
 	return local.NewStream(ctx, cfg)
