d6d7a5d11d71566d8e4ed5c26145cfb406af8bac
xymaxim <xymaxim@users.noreply.github.com>
2026-07-10 15:29:15 +0000

    deploy: 02806d6db32d97d202027459edf48f37a79b08b5

--- _app/immutable/entry/start.LOKPUiVg.js
+++
@@ -1,1 +0,0 @@
-import{o as e,t}from"../chunks/DfdM_xHg.js";export{e as load_css,t as start};
--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d0b535e3.js',
+                path_to_root + 'searchindex-49f51407.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
