b24f77bf38eff9bd5ca7ae2661d1b231bd2ab8ae
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2026-07-26 00:33:21 +0300

    refactor: remove containerEl var in WelcomePane

--- frontend/src/lib/components/WelcomePane.svelte
+++ frontend/src/lib/components/WelcomePane.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
  import WelcomeRows from "$lib/components/WelcomeRows.svelte";
  import WelcomePanneau from "$lib/components/WelcomePanneau.svelte";
-
- let containerEl = $state<HTMLDivElement>();
 </script>
 
 <div
     class="h-full w-full flex-col items-center justify-center rounded-2xl bg-[var(--rewyt-selected-lighter)]"
 >
-    <div bind:this={containerEl} class="h-full w-full">
+    <div class="flex h-full w-full items-center justify-center">
         <WelcomePanneau />
     </div>
     <div class="absolute inset-0 overflow-hidden">
