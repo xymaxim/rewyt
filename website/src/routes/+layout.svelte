9abe525fc45334b988288a73deb2190fdf8a9d45
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2026-07-29 23:23:54 +0300

    refactor(website): move header to the layout page

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { resolve } from "$app/paths";
+  import { Sparkles } from "lucide-svelte";
   import "../app.css";
   import favicon from "$lib/assets/favicon.svg";
 
@@ -6,4 +8,19 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<main class="relative mx-auto min-h-dvh max-w-7xl overflow-hidden">
+  <header
+    class="relative z-10 mt-4 flex w-full max-w-screen-xl justify-between px-4"
+  >
+    <a href={resolve("/")} class="flex items-center font-bold">Rewyt</a>
+    <nav class="flex items-center gap-6 text-sm font-medium">
+      <a href={resolve("/picks")} class="flex items-center gap-2"
+        ><Sparkles size={16} class="text-orange-400" /> Live picks</a
+      >
+      <a href="https://xymaxim.github.io/rewyt/docs/">Docs</a>
+      <a href="https://github.com/xymaxim/rewyt/">Source</a>
+    </nav>
+  </header>
+  {@render children()}
+</main>
--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,63 +1,49 @@
 <script lang="ts">
-  import { ArrowRight, Rewind, Play, Sparkles } from "lucide-svelte";
+  import { ArrowRight, Rewind, Play } from "lucide-svelte";
   import LandingPane from "/src/lib/components/LandingPane.svelte";
 
   let playing = $state(false);
   let rewinding = $state(false);
 </script>
 
-<main class="relative mx-auto min-h-dvh max-w-7xl overflow-hidden">
-  <header
-    class="relative z-10 mt-4 flex w-full max-w-screen-xl justify-between px-4"
+<div class="relative mt-8 flex justify-center">
+  <LandingPane bind:playing bind:rewinding />
+  <div
+    class="pointer-events-none absolute inset-x-0 top-[-40px] bottom-0 flex flex-col items-center justify-center gap-0 transition sm:gap-2"
+    class:opacity-0={rewinding || playing}
+    class:scale-50={rewinding || playing}
   >
+    <h1 class="mb-4 text-center text-3xl font-normal sm:text-5xl">
+      <p>Rewind and play</p>
+      <p>YouTube live streams</p>
+    </h1>
+    <p class="text-normal px-6 text-center">
+      Rewyt is a desktop app for rewatching live streams beyond YouTube's limits
+    </p>
+  </div>
+</div>
 
-<div class="flex items-center font-bold">Rewyt</div>
-    <nav class="flex items-center gap-6 text-sm font-medium">
-      <a class="flex items-center gap-2"
-        ><Sparkles size={16} class="text-orange-400" /> Live picks</a
-      >
-      <a href="https://xymaxim.github.io/rewyt/docs/">Docs</a>
-      <a href="https://github.com/xymaxim/rewyt/">Source</a>
-    </nav>
-  </header>
-
-  <div class="relative mt-8 flex justify-center">
-    <LandingPane bind:playing bind:rewinding />
-    <div
-      class="pointer-events-none absolute inset-x-0 top-[-40px] bottom-0 flex flex-col items-center justify-center gap-0 sm:gap-2 transition"
-      class:opacity-0={rewinding || playing}
-      class:scale-50={rewinding || playing}
+<div class="mt-12 flex flex-col items-center gap-4">
+  <div class="flex items-center gap-4">
+    <a
+      href="https://xymaxim.github.io/rewyt/docs/guides/install/desktop.html"
+      class="flex cursor-pointer items-center gap-1 rounded-2xl bg-neutral-200 px-4 py-2.5 text-sm font-semibold transition hover:scale-105 active:scale-95"
     >
-      <h1 class="mb-4 text-center font-normal text-3xl sm:text-5xl">
-        <p>Rewind and play</p>
-        <p>YouTube live streams</p>
-      </h1>
-      <p class="text-center text-normal px-6">
-        Rewyt is a desktop app for rewatching live streams beyond YouTube's limits
-      </p>
-    </div>
+      Download <ArrowRight />
+    </a>
+    <a
+      href="https://xymaxim.github.io/rewyt/docs/quickstart.html"
+      class="cursor-pointer rounded-2xl px-4 py-3 text-sm font-semibold transition hover:scale-105 active:scale-95"
+      >Get Started</a
+    >
   </div>
-
-  <div class="mt-12 flex flex-col items-center gap-4">
-    <div class="flex items-center gap-4">
-      <a
-        href="https://xymaxim.github.io/rewyt/docs/guides/install/desktop.html"
-        class="flex cursor-pointer items-center gap-1 rounded-2xl bg-neutral-200 px-4 py-2.5 text-sm font-semibold transition hover:scale-105 active:scale-95"
-      >
-        Download <ArrowRight />
-      </a>
-      <a
-        href="https://xymaxim.github.io/rewyt/docs/quickstart.html"
-        class="cursor-pointer rounded-2xl px-4 py-3 text-sm font-semibold transition hover:scale-105 active:scale-95"
-        >Get Started</a
-      >
-    </div>
-    <div class="flex flex-col items-center gap-2">
-      <p class="text-sm">Available for Linux, macOS, and Windows</p>
-      <p class="text-muted-foreground text-xs">
-        Read <a href="https://xymaxim.github.io/rewyt/docs/disclaimer.html" class="text-foreground cursor-pointer font-medium">this</a> before
-        using
-      </p>
-    </div>
+  <div class="flex flex-col items-center gap-2">
+    <p class="text-sm">Available for Linux, macOS, and Windows</p>
+    <p class="text-muted-foreground text-xs">
+      Read <a
+        href="https://xymaxim.github.io/rewyt/docs/disclaimer.html"
+        class="text-foreground cursor-pointer font-medium">this</a
+      > before using
+    </p>
   </div>
-</main>
+</div>
